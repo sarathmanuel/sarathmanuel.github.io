f46c84c0a1c59c130b50e2e3b22e15f17b9b264c
Kenneth Lin <kenworthe@gmail.com>
2017-01-27 00:41:25 -0500

    laid out routes skeleton in index.js

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,16 @@ var passport = require('passport');
 var User = require('../models/user.js');
 var jwt = require('express-jwt');
 
+// create middleware to authenticate user's JWT.
+// MUST CHANGE 'secret' LATER TO AN ENVIRONMENT VARIABLE.
 var auth = jwt({ secret: 'SECRET', userProperty: 'payload' });
 
-/* GET home page. */
+// GET home page.
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+// POST user register, respond with JWT.
 router.post('/register', function(req, res, next){
 	console.log('Posting new user:', req.body);
 	if (!req.body.username || !req.body.password){
@@ -29,10 +32,11 @@ router.post('/register', function(req, res, next){
 			else {
 				return res.status(401).json(info);
 			}
-		})(req, res, next);		//what the fuck does this line do..?
+		})(req, res, next);		// what the does this line do? is this an IIFE?
 	}
 });
 
+// POST user login, respond with JWT.
 router.post('/login', function(req, res, next){
 	console.log('>>>POST - Login attempt:', req.body);
 	if(!req.body.emailOrUsername || !req.body.password){
@@ -48,8 +52,31 @@ router.post('/login', function(req, res, next){
 			else {
 				return res.status(401).json(info);
 			}
-		})(req, res, next);		//what the fuck does this line do..?
+		})(req, res, next);		// what the does this line do? is this an IIFE?
 	}
+});
+
+// route for posting comments or replies. needs auth
+router.post ('/comment', auth, function(req, res, next){
+	console.log('>>>POST for new comment received: req.body: ', req.body);
+});
+
+// route for deleting comments. needs auth
+router.delete('/comment', auth, function(req, res, next){
+	console.log('>>>DELETE comment: req.body:', req. body);
+});
+
+// route for editing user's profile. needs auth. then serve new JWT (?).
+router.post('/user', auth, function(req, res, next){
+	// for edit username or email: run mongoose check.
+	// for edit password, need confirmPassword validation.
+	console.log('>>>Editing user: req.body:', req.body)
 })
+
+// route for deleting user's account. needs auth. then delete JWT.
+router.delete('/user', auth, function(req, res, next){
+	console.log('>>>DELETE user: req.body:', req. body);
+});
+
 
 module.exports = router;
